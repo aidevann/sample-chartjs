be7f26ba8243b3f96e62682eaa3d8e95f75224f3
Rh Dev <rhezannalbarracin.rc@gmail.com>
2017-10-04 11:33:17 +0800

    Add Polar Area chart

--- custom-chart.js
+++ custom-chart.js
@@ -287,3 +287,46 @@ var myChart7 = new Chart(ctx7, {
         }
     }
 });
+
+
+var ctx8 = document.getElementById("myChart8");
+var myChart8 = new Chart(ctx8, {
+    type: 'polarArea',
+    data: {
+        labels: ["Pink", "Blue", "Yellow", "Green", "Purple", "Orange"],
+        datasets: [{
+            label: '# of Votes',
+            data: [12, 19, 3, 5, 2, 3],
+            backgroundColor: [
+                'pink',
+                'blue',
+                'yellow',
+                'green',
+                'purple',
+                'orange'
+            ],
+            borderColor: [
+                'rgba(255,99,132,1)',
+                'rgba(54, 162, 235, 1)',
+                'rgba(255, 206, 86, 1)',
+                'rgba(75, 192, 192, 1)',
+                'rgba(153, 102, 255, 1)',
+                'rgba(255, 159, 64, 1)'
+            ],
+            borderWidth: 1
+        }]
+    },
+    options: {
+        scales: {
+            xAxes:[{
+                stacked: true
+            }],
+            yAxes: [{
+                // ticks: {
+                //     beginAtZero:true
+                // }
+                stacked: true
+            }],
+        }
+    }
+});
