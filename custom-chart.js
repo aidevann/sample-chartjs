8ffe32eb5a60b9a56dae59625572ff2539cc6470
Rh Dev <rhezannalbarracin.rc@gmail.com>
2017-10-04 11:46:22 +0800

    Add Scatter Chart

--- custom-chart.js
+++ custom-chart.js
@@ -365,3 +365,68 @@ var myChart9 = new Chart(ctx9, {
         }
     }
 });
+
+
+var ctx10 = document.getElementById("myChart10");
+var myChart10 = new Chart(ctx10, {
+    type: 'scatter',
+    data: {
+        datasets: [{
+            label: 'Scatter Dataset',
+            data: [{
+                x: -10,
+                y: 0
+            }, {
+                x: 0,
+                y: 10
+            }, {
+                x: 10,
+                y: 5
+            }]
+        }]
+    },
+    options: {
+        scales: {
+            xAxes: [{
+                type: 'linear',
+                position: 'bottom'
+            }]
+        }
+    }
+});
+
+
+var ctx11 = document.getElementById("myChart11");
+var myChart11 = new Chart(ctx11, {
+    type: 'bar',
+    data: {
+      datasets: [{
+            label: 'Bar Dataset',
+            data: [10, 20, 30, 40],
+            backgroundColor: [
+                'rgba(255, 99, 132, 0.2)',
+                'rgba(54, 162, 235, 0.2)',
+                'rgba(255, 206, 86, 0.2)',
+                'rgba(75, 192, 192, 0.2)',
+                'rgba(153, 102, 255, 0.2)',
+                'rgba(255, 159, 64, 0.2)'
+            ],
+            borderColor: [
+                'rgba(255,99,132,1)',
+                'rgba(54, 162, 235, 1)',
+                'rgba(255, 206, 86, 1)',
+                'rgba(75, 192, 192, 1)',
+                'rgba(153, 102, 255, 1)',
+                'rgba(255, 159, 64, 1)'
+            ],
+          }, {
+            label: 'Line Dataset',
+            data: [50, 50, 50, 50],
+  
+            // Changes this dataset to become a line
+            type: 'line'
+          }],
+      labels: ['January', 'February', 'March', 'April']
+    },
+    options: options
+});
